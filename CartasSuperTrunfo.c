1efdcc35f1772e2e1fb1a180955a254a25f07413
Ruthe <vitoriaruthe76@gmail.com>
2025-09-17 14:12:59 +0000

    buffer resolvido e printf mais legível

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -23,8 +23,8 @@ int main() {
   char cidade_carta1[40];
   char cidade_carta2[40];
 
-  int id_carta1;
-  int id_carta2;
+  char id_carta1[5];
+  char id_carta2[5];
 
   int populacao_carta1; 
   int populacao_carta2;
@@ -58,41 +58,57 @@ int main() {
   cidade_carta2[strcspn(cidade_carta2, "\n")] = 0;
 
   printf("Insira o código da carta 1, que é a letra que representa o estado + um número de 01 a 04:");
-  scanf("%d", &id_carta1);
+  fgets(id_carta1, sizeof(id_carta1), stdin);
+  id_carta1[strcspn(id_carta1, "\n")] = 0;
 
   printf("Insira o código da carta 2, que é a letra que representa o estado + um número de 01 a 04:");
-  scanf("%d", &id_carta2);
+  fgets(id_carta2, sizeof(id_carta2), stdin);
+  id_carta2[strcspn(id_carta2, "\n")] = 0;
 
   printf("Insira a população total da carta 1:");
   scanf("%d", &populacao_carta1);
+  while (getchar() != '\n');
 
   printf("Insira a população total da carta 2:");
   scanf("%d", &populacao_carta2);
+  while (getchar() != '\n');
 
   printf("Insira a área total em Km² da carta 1:");
   scanf("%f", &area_carta1);
+  while (getchar() != '\n');
 
   printf("Insira a área total em Km² da carta 2:");
   scanf("%f", &area_carta2);
+  while (getchar() != '\n');
 
   printf("Insira o PIB da carta 1:");
   scanf("%f", &pib_carta1);
+  while (getchar() != '\n');
 
   printf("Insira o PIB da carta 2:");
   scanf("%f", &pib_carta2);
+  while (getchar() != '\n');
 
   printf("Insira o número de pontos turísticos da carta 1:");
   scanf("%d", &turismo_carta1);
+  while (getchar() != '\n');
 
   printf("Insira o número de pontos turísticos da carta 2:");
   scanf("%d", &turismo_carta2);
+  while (getchar() != '\n');
 
   // Área para exibição dos dados da cidade
-  printf("Carta 1: \n, Estado: %s\n, Cidade: %s\n, Código da carta: %d\n, População: %d\n, Área em Km²: %f\n, PIB: %f\n, Pontos Turísticos: %d\n", 
-  estado_carta1, cidade_carta1, id_carta1, populacao_carta1, area_carta1, pib_carta1, turismo_carta1);
+  printf("\n=== Carta 1 ===\n");
+  printf("Estado: %s\n", estado_carta1);
+  printf("Cidade: %s\n", cidade_carta1);
+  printf("Código da carta: %d\n", id_carta1);
+  printf("População: %d\n", populacao_carta1);
+  printf("Área em Km²: %.2f\n", area_carta1);
+  printf("PIB: %.2f\n", pib_carta1);
+  printf("Pontos Turísticos: %d\n", turismo_carta1);
 
-  printf("Carta 2: \n, Estado: %s\n, Cidade: %s\n, Código da carta: %d\n, População: %d\n, Área em Km²: %f\n, PIB: %f\n, Pontos Turísticos: %d\n", 
-  estado_carta2, cidade_carta2, id_carta2, populacao_carta2, area_carta2, pib_carta2, turismo_carta2);
+  //printf("Carta 2: \n, Estado: %s\n, Cidade: %s\n, Código da carta: %d\n, População: %d\n, Área em Km²: %f\n, PIB: %f\n, Pontos Turísticos: %d\n", 
+  //estado_carta2, cidade_carta2, id_carta2, populacao_carta2, area_carta2, pib_carta2, turismo_carta2);
 
 return 0;
 } 
